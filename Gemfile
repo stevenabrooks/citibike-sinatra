e185a0da6789314edb0e003d11ba8f8a2178890a
Steven Brooks <brooks.stevena@gmail.com>
2013-06-30 12:58:13 -0400

    completed the assignement which was fun

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'multi_json'
 
+
 group :development do
   gem 'shotgun'
 end
--- app.rb
+++ app.rb
@@ -18,5 +18,25 @@ module Citibike
       erb :home
     end
 
+    get '/form' do
+      erb :form
+    end
+
+    post '/form' do
+      erb :response
+    end
+
+    post '/map' do
+      @start = params["start"].split(",").collect do |coords|
+        coords.to_f / 1000000
+      end
+      
+      @end = params["end"].split(",").collect do |coords|
+        coords.to_f / 1000000
+      end
+      
+      erb :map
+    end
+
   end
 end
